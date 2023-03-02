9a65113671a8055137e1447d39277517da6c6224
suk13574 <suk13574@naver.com>
2023-03-02 22:34:21 +0900

    프로젝트 생성

---
+++ src/test/java/com/app/univchat/UnivchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.univchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
