db3668651456a31bd5c44ee38eb01b6784cf55e6
dalkkommi <dalkkommi@gmail.com>
2023-03-16 17:39:12 +0900

    JwtFilter 에서 SecurityContext 에 세팅한 유저 정보를 꺼내기

---
+++ src/main/java/com/app/univchat/config/SecurityUtil.java
@@ -0,0 +1,23 @@
+package com.app.univchat.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Slf4j
+public class SecurityUtil {
+
+    private SecurityUtil() { }
+
+    // jwtFilter에서 SecurityContext에 세팅한 유저정보 꺼내기
+    // Id를 꺼내서 Long 타입으로 파싱하여 반환
+    public static Long getCurrentMemberId() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getName() == null) {
+            throw  new RuntimeException("Security Context 에 인증 정보가 없습니다.");
+        }
+
+        return Long.parseLong(authentication.getName());
+    }
+}
