dd646f518495b897b02233cd12868391835a9aed
dalkkommi <dalkkommi@gmail.com>
2023-03-16 17:36:30 +0900

    Security 로직에 jwt filter 붙이기 jwt provider 로직 구현 이후 결합 필요

---
+++ src/main/java/com/app/univchat/config/JwtSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.app.univchat.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+// custom TokenProvider, JwtFilter를 SecurityConfig에 적용
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final TokenProvider tokenProvider;  // jwt Token provider
+
+    // TokenProvider 를 주입받아서 JwtFilter 를 통해 Security 로직에 필터를 등록
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class); //custom jwt filter를 Security Filter 앞에 추가
+    }
+}
