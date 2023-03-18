4c5ed675496e06848d97bd06cb94361fb47232af
Donghwoo Cho <whehdgn1001@kookmin.ac.kr>
2023-03-18 13:19:54 +0900

    FIX: aws 자동 설정 클래스 예외 설정 #6
    빌드 시 에러를 해결하기 위해 예외 처리를 했습니다.

--- src/main/java/com/app/univchat/UnivchatApplication.java
+++ src/main/java/com/app/univchat/UnivchatApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class
-	})
+		DataSourceAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+})
 public class UnivchatApplication {
 
 	public static void main(String[] args) {
