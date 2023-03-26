5fc32e916153bb80fe39d6f4afddf3f17109b0ca
suk13574 <suk13574@naver.com>
2023-03-26 18:05:53 +0900

    ADD: UserDetails, UserDetailsService 추가 #15

--- src/main/java/com/app/univchat/repository/MemberRepository.java
+++ src/main/java/com/app/univchat/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package com.app.univchat.repository;
 import com.app.univchat.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    
+
+    Optional<Member> findByEmail(String email);
 }
---
+++ src/main/java/com/app/univchat/security/auth/PrincipalDetails.java
@@ -0,0 +1,53 @@
+package com.app.univchat.security.auth;
+
+import com.app.univchat.domain.Member;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+public class PrincipalDetails implements UserDetails {
+
+    private Member member;
+
+    public PrincipalDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
