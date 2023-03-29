2c797c1a9f5b640a2d795a80f1df3beb4bc64ed9
Donghwoo Cho <whehdgn1001@kookmin.ac.kr>
2023-03-29 16:37:28 +0900

    CHORE: 테스트 주석 처리 해제 #16

--- src/test/java/com/app/univchat/UnivchatApplicationTests.java
+++ src/test/java/com/app/univchat/UnivchatApplicationTests.java
@@ -6,10 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UnivchatApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//
-//	}
+	@Test
+	void contextLoads() {
+
+	}
 
 
 
