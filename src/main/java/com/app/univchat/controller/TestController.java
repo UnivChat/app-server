953441ce9458d3607634c11ced28379d8a3ddb29
Donghwoo Cho <whehdgn1001@kookmin.ac.kr>
2023-03-29 16:49:59 +0900

    CHORE: test api 허용 메소드 변경 #16

--- src/main/java/com/app/univchat/controller/TestController.java
+++ src/main/java/com/app/univchat/controller/TestController.java
@@ -2,12 +2,13 @@ package com.app.univchat.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("success");
     }
