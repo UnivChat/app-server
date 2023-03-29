73ce79d2b2447e7995d93cdcb445681448c3fa7d
dalkkommi <dalkkommi@gmail.com>
2023-03-29 12:07:26 +0900

    CHORE: passwordEncoder -> BCryptPasswordEncoder 객체 생성하는 방향으로 변경

--- src/main/java/com/app/univchat/service/MemberJoinService.java
+++ src/main/java/com/app/univchat/service/MemberJoinService.java
@@ -1,26 +1,33 @@
 package com.app.univchat.service;
 
 import com.app.univchat.dto.MemberJoinDTO;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+@Service
+@AllArgsConstructor
 public class MemberJoinService {
     @Autowired
     private MemberRepository memberRepository;
-    private PasswordEncoder passwordEncoder;
+//    private PasswordEncoder passwordEncoder;
 
-    public MemberJoinService() {
-    }
+//    public MemberJoinService() {
+//    }
 
-    @Transactional
+//    @Transactional
     public boolean join(MemberJoinDTO memberJoinDTO) {
         String rawPassword=memberJoinDTO.getPassword();
-        // password를 암호화
+        // password 암호화 - BCryptPasswordEncoder 를 객체로 받아옴
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String encPassword=passwordEncoder.encode(rawPassword);
-
         memberJoinDTO.setPassword(encPassword);
+
+        // dto를 entitiy로 변환해 저장
         memberRepository.save(memberJoinDTO.toEntity());
         return true;
     }
