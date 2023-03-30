b211835d4d17f7098c835e304d8744d8c6d98a96
Donghwoo Cho <whehdgn1001@kookmin.ac.kr>
2023-03-30 16:57:55 +0900

    CHORE: 테스트를 위한 코드 제거 #16
    이슈 해결함에 따라 테스트를 위한 코드를 제거합니다.

--- src/main/java/com/app/univchat/config/SecurityConfig.java
+++ src/main/java/com/app/univchat/config/SecurityConfig.java
@@ -61,7 +61,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/webjars/**").permitAll()
                 .antMatchers("/v3/api-docs").permitAll()
                 .antMatchers("/hello/**").permitAll()
-                .antMatchers("/").permitAll()
 
                 .anyRequest().authenticated() //위의 api가 아닌 경로는 모두 jwt 토큰 인증을 해야 함
 
