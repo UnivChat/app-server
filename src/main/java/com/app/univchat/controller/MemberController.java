d3c5b15b9244051ff541f8254b915c4d1f80ff71
dalkkommi <dalkkommi@gmail.com>
2023-04-05 22:05:07 +0900

    MOD: BaseResponse 형식에 맞춰 return형 수정

--- src/main/java/com/app/univchat/controller/MemberController.java
+++ src/main/java/com/app/univchat/controller/MemberController.java
@@ -1,32 +1,42 @@
 package com.app.univchat.controller;
 
+import com.app.univchat.base.BaseEntity;
+import com.app.univchat.base.BaseException;
+import com.app.univchat.base.BaseResponse;
+import com.app.univchat.base.BaseResponseStatus;
 import com.app.univchat.dto.MemberDTO;
 import com.app.univchat.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.app.univchat.base.BaseResponseStatus.*;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/member")
 public class MemberController {
-    private  MemberService memberService;
+    private final MemberService memberService;
 
     // 회원가입
     @PostMapping("/signup")
-    public BaseResponse<MemberDTO> signup(MemberDTO memberDTO) throws  BaseException{
+    public BaseResponse<MemberDTO> signup(MemberDTO memberDTO) throws BaseException {
         try {
             // 이메일 중복 체크
             if(memberService.checkEmail(memberDTO.getEmail())) {
-                return new BaseResponse<>(USER_ALREADY_EXIST_USERNAME);
+                return BaseResponse.ok(BaseResponseStatus.USER_ALREADY_EXIST_USERNAME);
             }
             // 닉네임 중복 체크
             if(memberService.checkNickname(memberDTO.getNickname())) {
-                return new BaseResponse<>(USER_EXISTS_NICKNAME_ERROR);
+                return BaseResponse.ok(BaseResponseStatus.USER_EXISTS_NICKNAME_ERROR);
             }
 
-            return new BaseResponse<>(memberService.signup(memberDTO));
+//            MemberDTO responseDTO=memberService.signup(memberDTO);
+            return BaseResponse.ok(SUCCESS,memberService.signup(memberDTO));
         } catch(BaseException e) {
-            return new BaseResponse<>((e.getStatus()));
+            return BaseResponse.ok(e.getStatus());
         }
 
     }
