9aeeb1644765513c2dbea331429d8255082509ae
dalkkommi <dalkkommi@gmail.com>
2023-04-11 18:42:35 +0900

    MOD: RequestBody 어노테이션 추가

--- src/main/java/com/app/univchat/controller/MemberController.java
+++ src/main/java/com/app/univchat/controller/MemberController.java
@@ -5,24 +5,33 @@ import com.app.univchat.base.BaseException;
 import com.app.univchat.base.BaseResponse;
 import com.app.univchat.base.BaseResponseStatus;
 import com.app.univchat.dto.MemberDTO;
+import com.app.univchat.repository.MemberRepository;
 import com.app.univchat.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 import static com.app.univchat.base.BaseResponseStatus.*;
 
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @RestController
 @RequestMapping("/member")
 public class MemberController {
+//    @Autowired
     private final MemberService memberService;
+//    private final MemberRepository memberRepository;
+
+    public MemberController(MemberService memberService) {
+        this.memberService=memberService;
+    }
 
     // 회원가입
+
     @PostMapping("/signup")
-    public BaseResponse<String> signup(MemberDTO memberDTO) throws BaseException {
+    public BaseResponse<String> signup(@RequestBody MemberDTO memberDTO) throws BaseException {
         try {
             // 이메일 중복 체크
             if(memberService.checkEmail(memberDTO.getEmail())) {
@@ -34,10 +43,18 @@ public class MemberController {
             }
 
 //            MemberDTO responseDTO=memberService.signup(memberDTO);
-            return BaseResponse.ok(SUCCESS,memberService.signup(memberDTO));
+            return BaseResponse.ok(BaseResponseStatus.SUCCESS,memberService.signup(memberDTO));
         } catch(BaseException e) {
+
             return BaseResponse.ok(e.getStatus());
         }
 
+
     }
+
+//    @GetMapping("/1")
+//    public ResponseEntity<String> memberTest() {
+//        return ResponseEntity.ok("memberTest");
+//    }
+
 }
