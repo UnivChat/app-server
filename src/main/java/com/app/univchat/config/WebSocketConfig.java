5b24d6efc745b55ea9fba78884c603a766e62c5d
Donghwoo Cho <whehdgn1001@kookmin.ac.kr>
2023-04-18 00:06:14 +0900

    ADD: 웹소켓 설정 파일 추가 #21
    웹소켓 설정을 위한 파일을 추가합니다. jwt 유효성 검증을 위한 인터셉터, 에러 핸들러를 등록했고 웹소켓 접속 앤드 포인트, 송수신을 위한 prefix 등을 설정했습니다.

--- src/main/java/com/app/univchat/config/WebSocketConfig.java
+++ src/main/java/com/app/univchat/config/WebSocketConfig.java
@@ -1,17 +1,40 @@
 package com.app.univchat.config;
 
+import com.app.univchat.chat.ChatExceptionHandler;
+import com.app.univchat.chat.ChatJwtInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final ChatJwtInterceptor chatInterceptor;
+    private final ChatExceptionHandler chatExceptionHandler;
+
+    // 메시지 송수신을 위한 url prefix 설정 및 메시지 브로커 설정
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new SocketTextHandler(), "/user") //소켓 엔드포인드 TODO 이름 설정 필요??
-                .setAllowedOrigins("*")
-                .withSockJS();
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub"); // 송신(/pub으로 시작하는 message는 @MessageMapping 메소드로 라우팅)
+        registry.enableSimpleBroker("/sub"); // 수신(/sub로 시작하는 url로 해당 메시지를 수신할 수 있음)
+    }
+
+    // 웹소켓 접속 앤드 포인트 설정, 웹소켓 허용 주소 설정
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.setErrorHandler(chatExceptionHandler); // 연결 시 예외 처리
+        registry.addEndpoint("/chat") // websocket 엔드포인트
+                .setAllowedOrigins("http://localhost:8080/*")
+                .withSockJS(); // websocket 방식으로 안 될 경우 sockJS를 사용하여 연결
+    }
+
+    // 웹소켓 연결 후, 채팅 중 Jwt 인증을 위한 intercept 설정
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(chatInterceptor);
     }
 }
