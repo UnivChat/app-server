81f68f01e806b3867b47da79cead8041d33967ed
YuJin Kwon <tokyj515@naver.com>
2023-05-29 19:06:16 +0900

    MOD: 연애 채팅 api 불러오기 리팩토링 #67

--- src/main/java/com/app/univchat/controller/chat/LoveChatController.java
+++ src/main/java/com/app/univchat/controller/chat/LoveChatController.java
@@ -20,6 +20,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import static com.app.univchat.base.BaseResponseStatus.CHAT_OVERFLOW_THE_RANGE;
+
 @Tag(name = "chatting", description = "채팅 내역 조회 API")
 @Controller
 @RequiredArgsConstructor
@@ -45,11 +47,15 @@ public class LoveChatController {
 
     // 기숙사 채팅 내역을 불러오기 위한 API(http)
     @Tag(name = "chatting")
-    @ApiOperation(value = "기숙사 채팅 내역 API", notes = "채팅 내역 최신순으로 10개를 반환하며, 페이지 번호는 0부터 시작합니다.")
+    @ApiOperation(value = "연애 상담 채팅 내역 API", notes = "채팅 내역 최신순으로 10개를 반환하며, 페이지 번호는 0부터 시작합니다.")
     @GetMapping("/chat/{page}")
-    public ResponseEntity<BaseResponse<List<ChatRes.LoveChatRes>>>loadLoveChattingList(@PathVariable int page) {
+    public ResponseEntity<BaseResponse<ChatRes.LoveChatListRes>>loadLoveChattingList(@PathVariable int page) {
 
-        List<ChatRes.LoveChatRes> chattingList = loveChatService.getChattingList(page);
+        ChatRes.LoveChatListRes chattingList = loveChatService.getChattingList(page, 10);
+
+        if(chattingList == null)
+            return ResponseEntity.ok(BaseResponse.ok(CHAT_OVERFLOW_THE_RANGE));
+
 
         return ResponseEntity.ok(BaseResponse.ok(BaseResponseStatus.SUCCESS, chattingList));
     }
--- src/main/java/com/app/univchat/service/LoveChatService.java
+++ src/main/java/com/app/univchat/service/LoveChatService.java
@@ -46,17 +46,39 @@ public class LoveChatService {
     /**
      *  연애상담 채팅 내역 조회
      */
-    public List<ChatRes.LoveChatRes> getChattingList(int page) {
+    public ChatRes.LoveChatListRes getChattingList(int page, int size) {
+
+        int all = loveChatRepository.findAll().size();
+        //총 페이지수 0~maxPage, 0이 최신
+        int maxPage = all / 10;
+
+        if(page > maxPage) return null;
+
+        if(page == -1){
+            // page는 요청하는 곳에 맞게, 한 번의 요청에는 10개의 채팅, 시간 내림차순으로 정렬.
+            Pageable pageable = PageRequest.of(0, size,
+                    Sort.by("messageSendingTime").descending());
+
+            // pagenation 한 채팅 목록을 modleMapper로 변환하여 반환
+            return new ChatRes.LoveChatListRes(maxPage, 0,
+                    loveChatRepository.findAll(pageable)
+                            .stream()
+                            .map(chat -> modelMapper.map(chat, ChatRes.LoveChatRes.class))
+                            .collect(Collectors.toList()));
+
+
+        }
 
         // page는 요청하는 곳에 맞게, 한 번의 요청에는 10개의 채팅, 시간 내림차순으로 정렬.
-        Pageable pageable = PageRequest.of(page, 10,
-                                    Sort.by("messageSendingTime").descending());
-        
+        Pageable pageable = PageRequest.of(page, size,
+                Sort.by("messageSendingTime").descending());
+
         // pagenation 한 채팅 목록을 modleMapper로 변환하여 반환
-        return loveChatRepository
-                        .findAll(pageable)
+        return new ChatRes.LoveChatListRes(maxPage, page,
+                loveChatRepository.findAll(pageable)
                         .stream()
                         .map(chat -> modelMapper.map(chat, ChatRes.LoveChatRes.class))
-                        .collect(Collectors.toList());
+                        .collect(Collectors.toList()));
+
     }
 }
