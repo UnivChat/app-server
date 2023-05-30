5bfa6a761dbb9e1208fd73e83732b91cfd625e19
Donghwoo Cho <whehdgn1001@kookmin.ac.kr>
2023-05-30 12:01:47 +0900

    ADD: 사용자 식별자를 이용하여 참여 중인 채팅방 목록 조회 커맨드 추가 #78

--- src/main/java/com/app/univchat/repository/OTOChatRoomRepository.java
+++ src/main/java/com/app/univchat/repository/OTOChatRoomRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OTOChatRoomRepository extends JpaRepository<OTOChatRoom, Long> {
@@ -19,6 +20,10 @@ public interface OTOChatRoomRepository extends JpaRepository<OTOChatRoom, Long> 
 
     // 송신자, 수신자로 채팅방 찾기
     Optional<OTOChatRoom> findBySenderAndReceive(Member sender, Member receive);
+
+    // 사용자로 채팅방 찾기
+    // TODO: 같은 값인데, 1개로 할 수는 없을까?
+    List<OTOChatRoom> findBySenderOrReceive(Member member1, Member member2);
 
     boolean existsBySenderAndReceive(Member sender, Member receive);
 
