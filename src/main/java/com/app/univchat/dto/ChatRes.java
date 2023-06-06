6d86cdc3551b8e9c753193eafe08aa3e7c5e8d33
Donghwoo Cho <whehdgn1001@kookmin.ac.kr>
2023-06-06 15:16:59 +0900

    REFACTOR: ChatRes 추상화 #83

--- src/main/java/com/app/univchat/dto/ChatRes.java
+++ src/main/java/com/app/univchat/dto/ChatRes.java
@@ -3,99 +3,46 @@ package com.app.univchat.dto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
-import java.util.List;
-
+@Setter
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatRes {
 
-    @Builder
-    @NoArgsConstructor
+    protected String memberNickname;
+    protected String messageContent;
+    protected String messageSendingTime;
+
+    @SuperBuilder
     @AllArgsConstructor
     @Setter
     @Getter
     // 기숙사 채팅 메시지 객체
-    public static class DormChatRes {
+    public static class DormChatRes extends ChatRes { }
 
-        private String memberNickname; // 송신자 식별자
-        private String messageContent;
-        private String messageSendingTime;
-    }
-
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Setter
-    @Getter
-    // 라이브 채팅 메시지 객체
-    public static class DormChatListRes {
-        private int maxPage;
-        private int nowPage;
-        private List<DormChatRes> dormChatRes;
-    }
-
-    @Builder
-    @NoArgsConstructor
+    @SuperBuilder
     @AllArgsConstructor
     @Setter
     @Getter
     // 연애상담 채팅 메시지 객체
-    public static class LoveChatRes {
-        private String memberNickname; // 송신자 식별자
-        private String messageContent;
-        private String messageSendingTime;
-    }
+    public static class LoveChatRes extends ChatRes { }
 
-    @Builder
-    @NoArgsConstructor
+    @SuperBuilder
     @AllArgsConstructor
     @Setter
     @Getter
     // 라이브 채팅 메시지 객체
-    public static class LoveChatListRes {
-        private int maxPage;
-        private int nowPage;
-        private List<LoveChatRes> loveChatRes;
-    }
+    public static class LiveChatRes extends ChatRes { }
 
-
-
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Setter
-    @Getter
-    // 라이브 채팅 메시지 객체
-    public static class LiveChatRes {
-        private String memberNickname; // 송신자 식별자
-        private String messageContent;
-        private String messageSendingTime;
-    }
-
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Setter
-    @Getter
-    // 라이브 채팅 메시지 객체
-    public static class LiveChatListRes {
-        private int maxPage;
-        private int nowPage;
-        private List<LiveChatRes> liveChatRes;
-    }
-
-
-
-    @Builder
-    @NoArgsConstructor
+    @SuperBuilder
     @AllArgsConstructor
     @Setter
     @Getter
     // 1:1 채팅 메시지 객체
-    public static class OTOChatRes {
-        private String memberNickname; // 송신자 식별자
-        private String messageContent;
-        private String messageSendingTime;
-    }
+    public static class OTOChatRes extends ChatRes { }
 
     @Builder
     @NoArgsConstructor
