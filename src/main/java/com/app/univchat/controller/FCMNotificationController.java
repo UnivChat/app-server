40d8ed0bd798303575afffebf04d1a265069ad07
dalkkommi <dalkkommi@gmail.com>
2023-07-08 00:02:12 +0900

    MOD: 알림전송 api error code 해결

--- src/main/java/com/app/univchat/controller/FCMNotificationController.java
+++ src/main/java/com/app/univchat/controller/FCMNotificationController.java
@@ -4,7 +4,6 @@ package com.app.univchat.controller;
 import com.app.univchat.base.BaseResponse;
 import com.app.univchat.base.BaseResponseStatus;
 import com.app.univchat.dto.FcmReq;
-import com.app.univchat.dto.FcmReq;
 import com.app.univchat.service.FCMNotificationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -12,6 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @Tag(name="Notification",description="FCM Notification api")
 @RequiredArgsConstructor
@@ -22,9 +24,7 @@ public class FCMNotificationController {
 
     @Operation(summary = "알림 보내기")
     @PostMapping("")
-    public BaseResponse<String> sendNotification(FcmReq.Notification fcmNotificationReq) {
-        if(fcmNotificationReq.getBody()==null) System.out.println("-------------------널널널-------------");
-//        else return "ture";
+    public BaseResponse<String> sendNotification(@RequestBody FcmReq.Notification fcmNotificationReq) {
 
         String res=fcmNotificationService.sendNotificationByToken(fcmNotificationReq);
 
--- src/main/java/com/app/univchat/dto/FcmReq.java
+++ src/main/java/com/app/univchat/dto/FcmReq.java
@@ -1,26 +1,13 @@
 package com.app.univchat.dto;
 
-import com.app.univchat.base.BaseException;
-import com.app.univchat.base.BaseResponseStatus;
-import com.app.univchat.domain.Fcm;
-import com.app.univchat.domain.Member;
-import com.app.univchat.domain.OTOChat;
-import com.app.univchat.domain.OTOChatRoom;
-import com.app.univchat.repository.MemberRepository;
 import com.sun.istack.NotNull;
 import lombok.*;
 
-import java.util.Optional;
-
-//@NoArgsConstructor
-//@Builder
-
 public class FcmReq {
 
-//    @Builder
+    @Builder
     @NoArgsConstructor
-//    @AllArgsConstructor
-//    @Setter
+    @AllArgsConstructor
     @Getter
     public static class Notification {
         @NotNull
@@ -29,40 +16,6 @@ public class FcmReq {
         private String title;
 
         private String body;
-
-//        public Long getTarget() {
-//            return target;
-//        }
-//
-//        public String getTitle() {
-//            return title;
-//        }
-//
-//        public String getBody() {
-//            return body;
-//        }
-
-
-        @Builder
-        public Notification(Long id,String title, String body) {
-            this.id=id;
-            this.title=title;
-            this.body=body;
-        }
-
-        // Dto를 entity 객체로 만들어 return해주는 메서드
-//        public Fcm toEntity(Optional<Member> member, String token) throws Exception {
-//            return Fcm.builder()
-//                    .member(member.orElseThrow(() -> new BaseException(BaseResponseStatus.USER_NOT_EXIST_ERROR)))
-//                    .token(token)
-//                    .build();
-//        }
     }
 
-//    @Builder
-//    public FCMNotificationReq(Long targetId,String title, String body) {
-//        this.targetId=targetId;
-//        this.title=title;
-//        this.body=body;
-//    }
 }
