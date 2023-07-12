b66aefae9b55aa82643c9ab1bb8f1d455ecc05d4
suk13574 <suk13574@naver.com>
2023-07-13 00:25:09 +0900

    ADD: 클래스 엔티티 생성 #104

---
+++ src/main/java/com/app/univchat/domain/ClassRoom.java
@@ -0,0 +1,37 @@
+package com.app.univchat.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@ToString
+public class ClassRoom {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String classNumber; //과목번호
+
+    @Column(nullable = false)
+    private String className; //교과목명
+
+    private String professor; //교수님
+
+    @Column(nullable = false)
+    private String classTime; //시간표
+
+    @Column(nullable = false)
+    private String  section;//이수구분
+
+    @Column(nullable = false)
+    private int credit; //학점
+
+    private String etc; //비고
+
+}
