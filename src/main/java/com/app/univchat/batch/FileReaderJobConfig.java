af65d970b6974145535b949b9d516eacbf36f351
suk13574 <suk13574@naver.com>
2023-07-16 23:41:03 +0900

    ADD: spring batch 의존성 추가 & 뼈대 작성 #104

---
+++ src/main/java/com/app/univchat/batch/FileReaderJobConfig.java
@@ -0,0 +1,43 @@
+package com.app.univchat.batch;
+
+import com.app.univchat.domain.ClassRoom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class FileReaderJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    private static final int chunkSize = 1000; //데이터 처리할 row size
+
+
+    /**
+     * Job - 배치 작업 단위, 여러 step이 존재할 수 있음
+     */
+    @Bean
+    public Job csvFileReaderJob(){
+        return jobBuilderFactory.get("csvFileReaderJob")
+                .start(csvFileReaderStep())
+                .build();
+    }
+
+    @Bean
+    public Step csvFileReaderStep(){
+        return stepBuilderFactory.get("csvFileReaderStep")
+                .<ClassRoom, ClassRoom>chunk(chunkSize) //<reader에서 읽을 타입, writer에 넘겨줄 타입>
+//                .reader()
+//                .processor()
+//                .writer()
+                .build();
+    }
+
+
+}
