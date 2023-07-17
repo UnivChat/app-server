a59d84053ea45bbb7c241f4e1a2f64228758d44b
suk13574 <suk13574@naver.com>
2023-07-17 12:02:34 +0900

    ADD: classRoom 엔티티에 학년 추가 #104

--- src/main/java/com/app/univchat/domain/ClassRoom.java
+++ src/main/java/com/app/univchat/domain/ClassRoom.java
@@ -17,15 +17,17 @@ public class ClassRoom {
     private String classNumber; //과목번호
 
     @Column(nullable = false)
-    private String className; //교과목명
+    private String className; //수업
 
-    private String professor; //교수님
-
-    @Column(nullable = false)
-    private String classTime; //시간표
+    private String professor; //교수
 
     @Column(nullable = false)
-    private String  section;//이수구분
+    private String  section;//구분
+
+    private int grade; //학년
+
+    @Column(nullable = false)
+    private String classTime; //시간
 
     @Column(nullable = false)
     private int credit; //학점
