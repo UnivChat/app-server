29543d19875e3bc55e158a9c17cd651c6af40dfd
suk13574 <suk13574@naver.com>
2023-07-23 17:20:15 +0900

    FEAT: 연락망 csv 파일 저장 #104

--- src/main/java/com/app/univchat/batch/FileReaderJobConfig.java
+++ src/main/java/com/app/univchat/batch/FileReaderJobConfig.java
@@ -1,6 +1,7 @@
 package com.app.univchat.batch;
 
 import com.app.univchat.dto.ClassRoomDto;
+import com.app.univchat.dto.school.PhoneDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -17,17 +18,29 @@ public class FileReaderJobConfig {
         private final StepBuilderFactory stepBuilderFactory;
         private final CsvReader csvReader;
         private final CsvClassWriter csvClassWriter;
+        private final CsvPhoneWriter csvPhoneWriter;
 
     private static final int chunkSize = 1000; //데이터 처리할 row size
 
 
     /**
      * Job - 배치 작업 단위, 여러 step이 존재할 수 있음
+     * 클래스 저장 Job
      */
     @Bean
     public Job csvFileReaderJob(){
         return jobBuilderFactory.get("csvFileReaderJob")
                 .start(csvFileReaderStep())
+                .build();
+    }
+
+    /**
+     * 연락망 저장 Job
+     */
+    @Bean
+    public Job csvPhoneReaderJob(){
+        return jobBuilderFactory.get("csvPhoneReaderJob")
+                .start(csvPhoneReaderStep())
                 .build();
     }
 
@@ -39,6 +52,17 @@ public class FileReaderJobConfig {
                 .writer(csvClassWriter)
 //                .allowStartIfComplete(true) //JobInstance COMPLETED 상태여도 재시작
                 .build();
+    }
+
+    @Bean
+    public Step csvPhoneReaderStep(){
+        return stepBuilderFactory.get("csvPhoneReaderStep")
+                .<PhoneDto, PhoneDto>chunk(chunkSize)
+                .reader(csvReader.csvPhoneReader())
+                .writer(csvPhoneWriter)
+//                .allowStartIfComplete(true)
+                .build();
+
     }
 
 
