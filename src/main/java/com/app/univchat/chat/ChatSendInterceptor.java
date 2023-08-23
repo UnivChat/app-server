d801a3e4002e161728e0aef7c556f201ce2a6791
suk13574 <suk13574@naver.com>
2023-08-23 20:32:07 +0900

    CHORE: 채팅 접속 시 닉네임, jwt 비교 주석처리 #122

--- src/main/java/com/app/univchat/chat/ChatSendInterceptor.java
+++ src/main/java/com/app/univchat/chat/ChatSendInterceptor.java
@@ -65,31 +65,9 @@ public class ChatSendInterceptor implements ChannelInterceptor {
         String email = jwtProvider.getEmail(jwt);
 
         // 닉네임 DB에 있는지 체크 & jwt와 일치하는지 확인
-        checkNickname(message, email);
+//        checkNickname(message, email);
 
         return message;
-    }
-
-    /**
-     * 닉네임 DB에 있는지 체크 & jwt와 일치하는지 확인
-     */
-    @SneakyThrows
-    public void checkNickname(Message<?> message, String email) {
-
-        // message payload에서 닉네임 얻기
-        String payload = new String((byte[])message.getPayload());
-        ChatReq.LiveChatReq body = Jackson.fromJsonString(payload, ChatReq.LiveChatReq.class);
-        System.out.println("sender nickname: " + body.getMemberNickname());
-
-        // DB에 닉네임 있는지 확인
-        Member member = memberService.getMember(body.getMemberNickname()).orElseThrow(
-                () -> new BaseException(BaseResponseStatus.USER_NOT_EXIST_NICKNAME_ERROR)
-        );
-
-        // jwt 토큰과 닉네임으로 찾은 member 객체 일치하는지 확인
-        if (!member.getEmail().equals(email)) {
-            throw new BaseException(BaseResponseStatus.JWT_AND_NICKNAME_DONT_MATCH);
-        }
     }
 
     /**
@@ -115,5 +93,28 @@ public class ChatSendInterceptor implements ChannelInterceptor {
             );
         }
 
+    }
+
+
+    /**
+     * 닉네임 DB에 있는지 체크 & jwt와 일치하는지 확인
+     */
+    @SneakyThrows
+    public void checkNickname(Message<?> message, String email) {
+
+        // message payload에서 닉네임 얻기
+        String payload = new String((byte[])message.getPayload());
+        ChatReq.LiveChatReq body = Jackson.fromJsonString(payload, ChatReq.LiveChatReq.class);
+        System.out.println("sender nickname: " + body.getMemberNickname());
+
+        // DB에 닉네임 있는지 확인
+        Member member = memberService.getMember(body.getMemberNickname()).orElseThrow(
+                () -> new BaseException(BaseResponseStatus.USER_NOT_EXIST_NICKNAME_ERROR)
+        );
+
+        // jwt 토큰과 닉네임으로 찾은 member 객체 일치하는지 확인
+        if (!member.getEmail().equals(email)) {
+            throw new BaseException(BaseResponseStatus.JWT_AND_NICKNAME_DONT_MATCH);
+        }
     }
 }
