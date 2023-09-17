c385f59bdbde5453ecc15fc435a0ea6c8c16f070
suk13574 <suk13574@naver.com>
2023-09-17 20:07:38 +0900

    MOD: 편의시설 조회 API - 건물 & 이름 조회 기능으로 수정 #140

--- src/main/java/com/app/univchat/controller/school/FacilityController.java
+++ src/main/java/com/app/univchat/controller/school/FacilityController.java
@@ -20,11 +20,17 @@ public class FacilityController {
     private final FacilityService facilityService;
 
     @Tag(name = "school", description = "학교 정보 관련 API")
-    @ApiOperation(value = "편의시설 전체 조회 API", notes = "편의시설 이름 검색 시 관련 편의시설 데이터만 반환")
-    @GetMapping("/facility")
-    public BaseResponse<List<FacilityDto>> getFacilityList(@RequestParam(required = false)String name) {
+    @ApiOperation(value = "편의시설 조회 API", notes = "조회 방법 3가지\n\n" +
+            "1. 전체 조회 - path에 아무것도 넣지 않고 조회 (ex. /school/facility) \n\n" +
+            "2. 건물별 조회 - path에 건물 기호 넣고 요청 (ex. /school/facility/K) \n\n" +
+            "   [K: 김수환관, N: 니콜스관, V: 비루투스관, D: 다솔관, B: 학생미래인재관, T: 미카엘관, L: 베리타스관, A: 안드레아관, BA: 밤비노관] \n\n" +
+            "3. 건물 + 이름 조회 - path에 건물 기호와 이름 넣고 요청 (ex. /school/facility/K/카페멘사)"
+            )
+    @GetMapping(value = {"/facility", "/facility/{building}", "/facility/{building}/{name}"})
+    public BaseResponse<List<FacilityDto>> getFacilityList(@PathVariable(name = "building", required = false)String building,
+                                                           @PathVariable(name = "name", required = false)String name) {
 
-        List<FacilityDto> result = facilityService.getFacilityList(name);
+        List<FacilityDto> result = facilityService.getFacilityList(building, name);
 
         return BaseResponse.ok(BaseResponseStatus.SUCCESS, result);
     }
